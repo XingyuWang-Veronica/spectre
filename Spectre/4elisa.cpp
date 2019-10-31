acdd49b9801d540fa2b5471093213cf88bac7989
Xingyu Wang <xingyuw@tubby.eecs.umich.edu>
2019-10-30 20:36:21 -0400

    Constant

--- Spectre/4elisa.cpp
+++ Spectre/4elisa.cpp
@@ -10,11 +10,11 @@ using namespace llvm;
 /*
  * A very specific pass that finds a gadget with the following pattern:
  * 
- * %1 = mul secret, 512 (or shl secret, 9)
- * %2 = add %1, array_addr 
- * %3 = inttoptr %2
+ * %1 = mul nsw %secret, 512 (or %1 = shl %secret, 9)
+ * %2 = sext i32 %1 to i64
+ * %2 = getelementptr inbounds i32, i32* getelementptr inbounds (...), i64 %2
  *
- *
+ * NOTE: there can be multiple store + load between 1 and 2, and between 2 and 3
  */
 namespace {
 	struct Spectre: public FunctionPass {
@@ -29,13 +29,30 @@ namespace {
 			for (BasicBlock &BB: F) {
 				for (Instruction &I: BB) {
 					std::string op = std::string(I.getOpcodeName());
+					bool find_mul = false;
 					if (op == "shl") {
 						// TODO: check
 						if (ConstantInt *CI = dyn_cast<ConstantInt>(I.getOperand(1))) {
-							errs() << "shl by a constant value\n";
+							// errs() << "shl by a constant value\n";
+							if (CI->getSExtValue() == 9) {
+								errs() << "shl by 9\n";
+								find_mul = true;
+							}
 						}
 					} else if (op == "mul") {
 						// TODO: check
+						if (ConstantInt *CI = dyn_cast<ConstantInt>(I.getOperand(1))) { 
+							// errs() << "mul by a constant value\n";
+							// errs() << "mul by " << CI->getSExtValue() << '\n';
+							if (CI->getSExtValue() == 512) {
+								errs() << "mul by 512\n";
+								find_mul = true;
+							}
+						}
+					}
+					if (find_mul) {
+						// next stage shall be getelementptr
+						// ignore: store + load, sext
 					}
 				}
 			}
